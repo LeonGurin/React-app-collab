d2d0c5d4b5a7deb309dbb5fe424a545b1ca77ecf
Leon Gurin <leon9gur@gmail.com>
2022-06-13 15:57:10 +0300

    +

---
+++ unnamed/src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+        <div className="navbar">
+            <nav>
+                <h2>LEON & OMER COLLAB</h2>
+                <a href="/">Home</a>
+                <a href="/Stuff">Stuff</a>
+                <a href="/BoboGaga">BoboGaga</a>
+            </nav>
+        </div>
+    );
+}
+ 
+export default Navbar;
