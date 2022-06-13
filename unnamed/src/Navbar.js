3e03784631d1d64dd189592b07cf05831fd0cf9d
Leon Gurin <leon9gur@gmail.com>
2022-06-13 17:09:58 +0300

    darkmode and navbar

--- unnamed/src/Navbar.js
+++ unnamed/src/Navbar.js
@@ -1,11 +1,23 @@
+
+const handleToggleDarkMode = () => {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
 const Navbar = () => {
     return ( 
         <div className="navbar">
             <nav>
                 <h2>LEON & OMER COLLAB</h2>
-                <a href="/">Home</a>
-                <a href="/Stuff">Stuff</a>
-                <a href="/BoboGaga">BoboGaga</a>
+                <li><a href="/">Home</a></li>
+                <li><a href="/Stuff">Stuff</a></li>
+                <li><a href="/BoboGaga">BoboGaga</a></li>
+                <li>
+                    <label class="switch">
+                        <input type="checkbox" onClick={handleToggleDarkMode}></input>
+                        <span class="slider round"></span>
+                    </label>
+                </li>
             </nav>
         </div>
     );
